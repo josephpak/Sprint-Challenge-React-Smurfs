289a6674fd99c6731b7231727d447b535e3bbff2
joepak <josephpak2014@gmail.com>
2019-03-01 11:59:50 -0500

    Stage 2 - Routes and MVP

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { NavLink, Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -27,8 +28,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink to='/'>Smurf List</NavLink>
+        <NavLink to='/smurf-form'>Add a Smurf</NavLink>
+
+        {/* Routes */}
+        <Route 
+        path='/smurf-form'
+        render={props => (
+          <SmurfForm
+            {...props}
+          />
+        )}
+        />
+        <Route 
+        exacx
+        path='/'
+        render={props => (
+          <Smurfs 
+            {...props}
+            smurfs={this.state.smurfs}
+          />
+        )}
+        />
+
       </div>
     );
   }
